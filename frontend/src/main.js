51c2816e18076d527545920f7f978c8159948aec
Hosoda Keiji <kei.2001.1031@gmail.com>
2025-06-18 18:54:31 +0900

    サイドバーコンポーネントエラーの修正

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,12 +3,18 @@ import App from './App.vue'
 import Signup from './components/Signup.vue'
 import Login from './components/Login.vue'
 import Home from "./components/Home.vue"
+import Daily from "./components/Daily.vue"
+import Weekly from "./components/Weekly.vue"
+import Monthly from "./components/Monthly.vue"
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
     { path: '/signup', component: Signup },
     { path: '/login', component: Login },
     { path: "/home", component: Home},
+    { path: "/daily", component: Daily},
+    { path: "/weekly", component: Weekly},
+    { path: "/monthly", component: Monthly},
 ]
 
 const router = createRouter({
